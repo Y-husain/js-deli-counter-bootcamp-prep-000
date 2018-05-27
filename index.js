5127494d6933a4b6a37c2f278dd800d8bf16a992
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 20:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,16 +7,7 @@ var takeANumber = function(katzDeli, newCustomer) {
      return ('Welcome, ' + newCustomer + '. You are number ' + katzDeli.length + ' in line.')
   }
   
-  
-  function nowServing (katzDeliLine) {
-  
-   if(katzDeliLine.length===0) {
-     
-   return 'There is nobody waiting to be served!';
-}
-   var customer = katzDeliLine.shift(); 
-   
-   return "Currently serving " + customer + "."; }
+
    
    
 function nowServing (katzDeliLine) {
@@ -38,4 +29,13 @@ function nowServing (katzDeliLine) {
 const deliLine = ["Steven", "Blake", "Avi"];
 console.log(deliLine);
 
-
+var currentLine = function (katzDeliLine) {
+  if (katzDeliLine.length===0) {
+  return "The line is currently empty.";
+}
+   var customerAndNumber = [];
+   for (var i=0; i<katzDeliLine.length; i++) {
+   customerAndNumber.push(i + 1 + ". " + katzDeliLine[i])
+}
+  return "The line is currently: " + customerAndNumber.join(", ");
+}
