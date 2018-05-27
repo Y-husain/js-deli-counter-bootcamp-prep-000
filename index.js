ab9b8ded81121bb3d24358433d241dab3ea22622
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 19:50:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var katzDeli = []
+// var katzDeli = []
 
-function takeANumber(katzDeli, name){
-  var number = 0
-  for(let i=0; i<number;)
-}
+// function takeANumber(katzDeli, name){
+//   var number = 0
+//   for(let i=0; i<number;)
+// }
