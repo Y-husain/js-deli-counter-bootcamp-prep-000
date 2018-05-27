983c87a6307acd4e7336b8e48ea7b09127c97f8c
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 20:03:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ var takeANumber = function(katzDeli, newCustomer) {
    var customer = katzDeliLine.shift(); 
    
    return "Currently serving " + customer + "."; }
+   
+     describe('nowServing', () => {
+    it('returns the line is empty when no one is on line', () => {
+      expect(nowServing([])).toEqual("There is nobody waiting to be served!");
+    });
+
+    it('returns an announcement about the person it is serving, and shifts the line', () => {
+      const deliLine = ["Steven", "Blake", "Avi"]
+      expect(nowServing(deliLine)).toEqual("Currently serving Steven.");
+      expect(deliLine).toEqual(["Blake", "Avi"]);
+    });
+  });
