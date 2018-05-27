3888fa0e63ecee08f3974d9832bb7b963664062f
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-27 19:54:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var katzDeli = []
 
 function takeANumber(katzDeli, name){
-  var number = 0
+  var number = 0;
   for(let i=0; i<number; i++1){
-    return "You are num"
+    return "You are num" $(name) 
   }
 }
